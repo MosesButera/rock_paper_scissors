472eb2835abdab85b93a9c6f1fd59c2b41ae9127
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-06-25 03:29:22 -0600

    Declare function get computerChoice to cycle through the randomNUm and return rock for 0, paper for 1 and scissors for 2.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 
 let randomNum = Math.floor(Math.random() * 3);
  
+function getComputerChoice(randomNum) {
+    if (randomNum == 0){
+        return "ROCK";
+    } else if (randomNum == 1){
+        return "PAPER";
+    } else {
+        return "SCISSORS";
+    }
+}
