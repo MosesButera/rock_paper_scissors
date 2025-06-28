9619d2629b2e215a54ff4cf101770bcb21c1488d
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-06-28 03:26:29 -0600

    Declare two variables to use later. Create function gethumanchoice to prompt the user to enter either ROCK, PAPER, or SCISORS and return input.
    Use the toUpperCase() to ensure output consistency this will be useful later. Use an if statement to output either ROCK, PAPER, SCISSORS.

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 
-let randomNum = Math.floor(Math.random() * 3); //Output should be limited to only 0,1,or 2 and nothing else.
- 
-function getComputerChoice(randomNum) {
-    if (randomNum == 0){
-        return "ROCK";
-    } else if (randomNum == 1){
-        return "PAPER";
-    } else {
-        return "SCISSORS";
-    }
+
+let computer;
+let result;
+
+function getHumanChoice (){
+    let PlayerChoice = prompt("Enter either ROCK, PAPER, or SCISSORS to play");
+    let uppercaseChoice = PlayerChoice.toUpperCase(); //This will account for difference in capitalization of user input
+
+    if (uppercaseChoice === "ROCK") return uppercaseChoice = "ROCK";
+    else if (uppercaseChoice === "PAPER") return uppercaseChoice = "PAPER";
+    else if (uppercaseChoice === "SCISSORS")return uppercaseChoice = "SCISSORS"; //Add condition for scissors
+    else if (uppercaseChoice === null ) return uppercaseChoice = "User canceled the prompt.";
+    else return uppercaseChoice = "Re-enter either ROCK, PAPER, or SCISSORS";   //Add condition for anything else
+    
 }
 
-console.log(getComputerChoice(randomNum));
 
-let choice = prompt("Enter either ROCK, PAPER, or SCISSORS to play");
 
-function getHumanChoice (choice){
-    if (choice == "ROCK") return "ROCK";
-    else if (choice == "PAPER") return "PAPER";
-    else return "SCISSORS";
-}
 
-console.log(getHumanChoice(choice));
 
-let humanScore = 0;
-let computerScore = 0; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
