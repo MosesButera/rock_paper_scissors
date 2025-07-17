a218a24c5a3c88a22fa4b03fb676ef5e4bcd7ac8
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-07-17 14:24:11 -0600

    Add function to randomly choose computer move ("ROCK", "PAPER", or "SCISSORS") for each round

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // 1. GAME STATE
 let humanScore = 0;
 let computerScore = 0;
+
+// 2. CORE GAME LOGIC
+
+function getComputerChoice() {
+    const computerOutputDiv = document.querySelector(".computerChoiceOutput");
+    const randomNum = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+    let choice;
+
+    if (randomNum === 0) {
+        choice = "ROCK";
+    } else if (randomNum === 1) {
+        choice = "PAPER";
+    } else {
+        choice = "SCISSORS";
+    }
+
+    computerOutputDiv.textContent = `Computer Choice: ${choice}`;
+    return choice;
+};
